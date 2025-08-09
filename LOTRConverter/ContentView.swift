9916a875fe4b2e130e66a6170fd3ca0557462b3a
dougmelo <douglasportmelo@gmail.com>
2025-08-09 04:53:11 -0300

    Initial Commit

---
+++ LOTRConverter/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Douglas de Melo Pontes on 09/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                HStack{
+                    VStack{
+                        HStack{
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 30)
+                            
+                            Text("Currency Exchange")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            
+                        }
+                        
+                        Text("Adiconar TextField")
+                        
+                    }
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    VStack{
+                        HStack{
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        Text("Adiconar TextField")
+                    }
+                }
+                
+                Spacer()
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            }
+//            .border(.blue)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Douglas de Melo Pontes on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
